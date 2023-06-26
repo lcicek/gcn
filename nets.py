79285b9ac7fcf20a3b1cd26679d3df10f2d2e13b
lcicek <lunay.cicek3@gmail.com>
2023-06-26 11:27:48 +0200

    Changed network structure slightly.

--- nets.py
+++ nets.py
@@ -1,24 +1,25 @@
 import torch
-from torch.nn import Sigmoid, LazyLinear
-from torch_geometric.nn import GCNConv, global_add_pool
+from torch.nn import Sigmoid, LazyLinear, Dropout
+from torch_geometric.nn import GCNConv, global_mean_pool
 
-from parameters import P_THRESHOLD
 from utility import label, labelDistance
 
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv = GCNConv(136, 136, improved=True)
-        self.conv2 = GCNConv(136, 136, improved=True)
+        self.conv = GCNConv(136, 50, improved=True)
+        self.conv2 = GCNConv(50, 50, improved=True)
         self.lin = LazyLinear(1)
         self.pred = Sigmoid()
+        self.drop = Dropout(0.2)
 
     def forward(self, x, edge_index, batch):
         x = self.conv(x, edge_index).relu()
         x = self.conv2(x, edge_index).relu()
+        x = global_mean_pool(x, batch) # readout layer
+        x = self.drop(x) # regularization layer
         x = self.lin(x)
-        x = global_add_pool(x, batch)
         x = self.pred(x).flatten()
 
         return x
