8176230e4068f168f15cb74e29d80d1cad237f23
lcicek <lunay.cicek3@gmail.com>
2023-06-26 13:17:57 +0200

    Changed epochs and learning rate.

--- parameters.py
+++ parameters.py
@@ -1,9 +1,7 @@
 BATCH_SIZE = 50
-EPOCHS = 150
-LEARNING_RATE = 0.01
+EPOCHS = 75
+LEARNING_RATE = 0.02
 
 P_THRESHOLD = 0.5 # probability threshold
 
 MODEL_PATH = f'saved-models/model.pt'
-
-# LOAD = False
