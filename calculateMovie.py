0080b5d38564bd1aebe6b5866462f41e15f46b63
lcicek <lunay.cicek3@gmail.com>
2023-06-30 14:41:20 +0200

    Added fixed movie array in own file.

---
+++ calculateMovie.py
@@ -0,0 +1,13 @@
+from symmetricGraph import symmetric_graph
+from utility import loadModel
+
+movie = []
+model = loadModel()
+for cast in range(0, 2):
+    movie.append(-1) # graphs of size 0 and 1 wouldn't make sense
+
+for cast in range(2, 31):
+    graph = symmetric_graph(num_movies=1, cast=cast, overlap=0)
+    pred = model(graph.x, graph.edge_index).item()
+
+    movie.append(pred)
