842b3577f63c9236f9af016488be456106240f01
lcicek <lunay.cicek3@gmail.com>
2023-07-01 13:33:48 +0200

    Increased size of fixed movie table.

--- calculateMovie.py
+++ calculateMovie.py
@@ -1,12 +1,13 @@
+# calculateMovie.py calculates the fixed values of differently sized movies
+
+
 from symmetricGraph import SymmetricGraph as sg
 from utility import loadModel
 
 movie = []
 model = loadModel()
-for cast in range(0, 2):
-    movie.append(-1) # graphs of size 0 and 1 wouldn't make sense
-
-for cast in range(2, 31):
+movie.append(0) # value = -1 for cast = 0
+for cast in range(1, 136): # 136 is max degree, so 135 is max cast
     graph = sg(num_movies=1, cast=cast, overlap=0)
     pred = model(graph.x, graph.edge_index).item()
 
